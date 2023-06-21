5124afe6f970459c6f9573952afc228f4e332584
M.Anas Khan <121577601+Bhoolay@users.noreply.github.com>
2023-06-22 04:06:52 +0500

    Add own weather api

--- server.mjs
+++ server.mjs
@@ -1,19 +1,60 @@
 import express from 'express';
-
+import cors from 'cors'
+import path from 'path';
+const __dirname = path.resolve();
 
 
 const app = express();
+// app.use(cors());
 
 
 
 //      http://192.168.0.104:3000
-app.get('/', (req, res) => {
+
+
+
+app.get('/weather/:CityName', (req, res) => {
     console.log('Hello World!', new Date());
-    res.send('In the realm of programming, Hello, World! stands as a universal language, transcending barriers and inviting collaboration across borders. ' + new Date());
+
+    let weatherData = { 
+        karachi: {
+            city: "Karachi",
+            TemoInC: 32,
+            Wind: 54,
+            high: 36,
+            low: 28
+        },
+
+        islamabad:{
+            city: "Islamabad",
+            TemoInC: 22,
+            Wind: 60,
+            high: 25,
+            low: 20
+        },
+    }
+
+    let UserInputCityName = req.params.CityName.toLowerCase();
+
+    let weatherDataToSend = weatherData[UserInputCityName]
+
+
+    if(weatherDataToSend){
+        res.send(weatherDataToSend);
+    }else{
+        res.status(404).send(`Weather is not avaiblable for ${req.params.CityName}`);
+    }
+
+
+
+    // res.send();
 })
 
+
+app.use('/', express.static(path.join(__dirname,  'public')))
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
 })
+
