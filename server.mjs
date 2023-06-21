335dd63fbd072d99720a568924a7b2db878ee00b
M.Anas Khan <121577601+Bhoolay@users.noreply.github.com>
2023-06-21 21:15:06 +0500

    PORT

--- server.mjs
+++ server.mjs
@@ -15,5 +15,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${PORT}`)
 })
